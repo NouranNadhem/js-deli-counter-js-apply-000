03e9526c85629cfa50f1ef917b28699822b315c2
nouran.nadhem@gmail.com Nadhem Haider <nouran.nadhem@gmail.com>
2018-07-24 05:00:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing (katzDeliLine) {
   return "Currently serving " + customer + "."; 
   }
   
-  var currentLine = function (katzDeliLine) {
+  varfunction currentLine =  (katzDeliLine) {
     if (katzDeliLine.length===0) {
       return "The line is currently empty."; 
       }
